3950cfb9c61b25976216c59506cd395127c2ab96
NextGoalIs <75665640+NextGoalIs@users.noreply.github.com>
2022-10-11 16:56:27 +0900

    Apply #1 - 스프링 Config 생성

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("./")
+@Configuration
+public class Config {
+
+
+
+}
